ffe53ff66e16812e9b5aabe3060f36155f531d9e
MarlonBrendonx <marlonbrendo2013@gmail.com>
2022-09-23 23:11:54 -0300

    Update: Update of used files

---
+++ Generator/generator.py
@@ -0,0 +1,141 @@
+import os
+import pdb
+import random
+
+import numpy as np
+import tensorflow as tf
+
+
+def savePasswords(passwords, num_generate, temperature):
+
+    file_name = 'passwords/passwords_'+str(num_generate) + '_'+str(temperature)+'.txt'
+
+    try:
+        passwd = open(file_name, 'w')
+        for i in passwords:
+            passwd.write(i)
+
+    except:
+        print('Erro open file')
+
+    passwd.close
+
+
+def generate_text(model, num_generate, temperature, start_string):
+
+    input_indices = [char2index[s] for s in start_string]
+    input_indices = tf.expand_dims(input_indices, 0)
+
+    text_generated = []
+
+    model.reset_states()
+    for char_index in range(num_generate):
+        predictions = model(input_indices)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # Using a categorical distribution to predict the character returned by the model.
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(
+            predictions,
+            num_samples=1
+        )[-1, 0].numpy()
+
+        input_indices = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(index2char[predicted_id])
+
+    savePasswords(text_generated, num_generate, temperature)
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+        batch_input_shape=[batch_size, None]
+    ))
+
+    # Mudar de acordo com o modelo
+
+    model.add(tf.keras.layers.LSTM(
+        units=rnn_units,
+        return_sequences=True,
+        stateful=True,
+        recurrent_initializer=tf.keras.initializers.GlorotNormal()
+    ))
+
+    model.add(tf.keras.layers.LSTM(
+        units=rnn_units,
+        return_sequences=True,
+        stateful=True,
+        recurrent_initializer=tf.keras.initializers.GlorotNormal()
+    ))
+
+    model.add(tf.keras.layers.Dense(vocab_size))
+
+    return model
+
+
+def getSeeds(lines, number_of_seeds):
+
+    seeds = []
+
+    for i in range(number_of_seeds):
+        seeds.append(random.choice(lines))
+
+    return '\n'.join(seeds)
+
+
+text = open('../train.txt', mode='r', encoding="latin-1").read()
+print('Length of text: {} characters'.format(len(text)))
+
+
+test = open('test.txt', mode='r', encoding="latin-1").read()
+lines = test.splitlines()
+
+seeds = getSeeds(lines, 15)
+
+print("Seeds: ", seeds)
+
+vocab = sorted(set(text))
+vocab_size = len(vocab)
+
+char2index = {char: index for index, char in enumerate(vocab)}
+index2char = np.array(vocab)
+print(index2char)
+
+
+embedding_dim = 256
+
+# Number of RNN units.
+rnn_units = 1024
+
+checkpoint_dir = 'tmp/checkpoints'
+
+tf.train.latest_checkpoint(checkpoint_dir)
+
+simplified_batch_size = 1
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([simplified_batch_size, None]))
+
+model.summary()
+
+
+num_generate = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+temperature = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
+               0.7, 0.8, 0.7, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+
+
+for num in num_generate:
+    for temp in temperature:
+        print('Number Generate: ', str(num) + '\n' +
+              'Temperature: ' + str(temp) + '\n')
+        print(seeds)
+        generate_text(model, num, temp, start_string=seeds)
--- password_generator.py
+++ password_generator.py
@@ -12,39 +12,25 @@ from keras.layers import Activation, Dense, Dropout
 from sklearn.model_selection import train_test_split
 
 
-# num_generate
-# - number of characters to generate.
-#
-# temperature
-# - Low temperatures results in more predictable text.
-# - Higher temperatures results in more surprising text.
-# - Experiment to find the best setting.
 def generate_text(model, start_string, num_generate = 1000, temperature=1.0):
-    # Evaluation step (generating text using the learned model)
 
-    # Converting our start string to numbers (vectorizing).
     input_indices = [char2index[s] for s in start_string]
     input_indices = tf.expand_dims(input_indices, 0)
 
-    # Empty string to store our results.
     text_generated = []
 
-    # Here batch size == 1.
     model.reset_states()
+
     for char_index in range(num_generate):
         predictions = model(input_indices)
-        # remove the batch dimension
         predictions = tf.squeeze(predictions, 0)
 
-        # Using a categorical distribution to predict the character returned by the model.
         predictions = predictions / temperature
         predicted_id = tf.random.categorical(
         predictions,
         num_samples=1
         )[-1,0].numpy()
 
-        # We pass the predicted character as the next input to the model
-        # along with the previous hidden state.
         input_indices = tf.expand_dims([predicted_id], 0)
 
         text_generated.append(index2char[predicted_id])
@@ -85,12 +71,11 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
       recurrent_initializer=tf.keras.initializers.GlorotNormal()
     ))
 
-
     model.add(tf.keras.layers.LSTM(
-      units=rnn_units,
-      return_sequences=True,
-      stateful=True,
-      recurrent_initializer=tf.keras.initializers.GlorotNormal()
+       units=rnn_units,
+       return_sequences=True,
+       stateful=True,
+       recurrent_initializer=tf.keras.initializers.GlorotNormal()
     ))
 
 
@@ -107,9 +92,6 @@ def split_input_target(chunk):
 text = open('train.txt', mode='r', encoding="latin-1").read()
 print('Length of text: {} characters'.format(len(text)))
 
-
-
-# The unique characters in the file
 vocab = sorted(set(text))
 
 print('{} unique characters'.format(len(vocab)))
@@ -119,38 +101,26 @@ char2index = {char: index for index, char in enumerate(vocab)}
 index2char = np.array(vocab)
 print(index2char)
 
-# Convert chars in text to indices.
 text_as_int = np.array([char2index[char] for char in text])
 
-sequence_length=100
-# Create training dataset.
+sequence_length=25
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
-# Generate batched sequences out of the char_dataset.
 sequences = char_dataset.batch(sequence_length + 1, drop_remainder=True)
 dataset = sequences.map(split_input_target)
 
-
-# Batch size.
 BATCH_SIZE = 32
 
-# Buffer size to shuffle the dataset (TF data is designed to work
-# with possibly infinite sequences, so it doesn't attempt to shuffle
-# the entire sequence in memory. Instead, it maintains a buffer in
-# which it shuffles elements).
 BUFFER_SIZE = 1000
 
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 print('Batched dataset size: {}'.format(len(list(dataset.as_numpy_iterator()))))
 
-# Length of the vocabulary in chars.
 vocab_size = len(vocab)
 
-# The embedding dimension.
 embedding_dim = 256
 
-# Number of RNN units.
-rnn_units = 1024
+rnn_units = 512
 
 model = build_model(vocab_size, embedding_dim, rnn_units, BATCH_SIZE)
 model.summary()
@@ -162,21 +132,12 @@ tf.keras.utils.plot_model(
 )
 
 
-adam_optimizer = tf.keras.optimizers.Adam(
-    learning_rate=0.001,
-    rho=0.9,
-    momentum=0.0,
-    epsilon=1e-07,
-    centered=False,
-    name="Adam"
-)
+adam_optimizer = tf.keras.optimizers.Adam(learning_rate=0.002)
 model.compile(optimizer=adam_optimizer, loss=loss,  metrics=['accuracy'])
 
-# Directory where the checkpoints will be saved.
 checkpoint_dir = 'tmp/checkpoints'
 os.makedirs(checkpoint_dir, exist_ok=True)
 
-# Name of the checkpoint files
 checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt_{epoch}')
 
 checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
@@ -185,6 +146,7 @@ checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
 )
 
 EPOCHS=1000
+
 history = model.fit(
   x=dataset,
   epochs=EPOCHS,
@@ -208,4 +170,3 @@ model.build(tf.TensorShape([simplified_batch_size, None]))
 
 model.summary()
 
-#print(generate_text(model, start_string=u"tilletiaceous"))
